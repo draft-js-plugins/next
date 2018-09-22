f845aec0f0aad8043342e9b90283c7033e7e17f6
Julian Krispel <julian@reactrocket.com>
2018-09-22 12:27:57 +0100

    - add djs-utils - finish mentions example

---
+++ packages/djs-utils/README.md
@@ -0,0 +1,31 @@
+# djs-utils
+
+> Utilities for draft js
+
+[![NPM](https://img.shields.io/npm/v/djs-utils.svg)](https://www.npmjs.com/package/djs-utils) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
+
+## Install
+
+```bash
+npm install --save djs-utils
+```
+
+## Usage
+
+```jsx
+import React, { Component } from 'react'
+
+import MyComponent from 'djs-utils'
+
+class Example extends Component {
+  render () {
+    return (
+      <MyComponent />
+    )
+  }
+}
+```
+
+## License
+
+MIT © [juliankrispel](https://github.com/juliankrispel)
---
+++ packages/djs-utils/src/index.js
@@ -0,0 +1,105 @@
+// @flow
+
+import React from 'react';
+import { RichUtils, EditorState, ContentState, SelectionState, Modifier } from 'draft-js';
+import type DraftEntityInstance from 'draft-js/lib/DraftEntityInstance';
+
+export function createEntityDecorator(
+  entityName: string,
+  component: React$Element,
+  props?: Object
+) {
+  return {
+    strategy: (contentBlock, callback, contentState) => {
+      contentBlock.findEntityRanges(
+        (character) => {
+          const entityKey = character.getEntity();
+          return (
+            entityKey !== null &&
+            contentState.getEntity(entityKey).getType() === entityName
+          );
+        },
+        callback
+      );
+    },
+    component,
+    props
+  }
+}
+
+export function insertTextWithEntity(
+  contentState: ContentState,
+  selection: SelectionState,
+  entityType: string,
+  text: string,
+  mutability?: 'IMMUTABLE' | 'MUTABLE' | 'SEGMENTED',
+  entityData?: Object,
+): ContentState {
+
+  const content = contentState.createEntity(
+    entityType,
+    mutability,
+    entityData
+  );
+
+  return Modifier.insertText(
+    content,
+    selection,
+    text,
+    null,
+    content.getLastCreatedEntityKey()
+  )
+}
+
+export function createLinkAtSelection(editorState: EditorState, url: string): EditorState {
+  const contentState = editorState.getCurrentContent().createEntity('LINK', 'MUTABLE', { url });
+  const entityKey = contentState.getLastCreatedEntityKey();
+  const withLink = RichUtils.toggleLink(
+    editorState,
+    editorState.getSelection(),
+    entityKey
+  );
+  return EditorState.forceSelection(
+    withLink, editorState.getSelection()
+  );
+}
+
+export function removeLinkAtSelection(editorState: EditorState): EditorState {
+  const selection = editorState.getSelection();
+  return RichUtils.toggleLink(editorState, selection, null);
+}
+
+export function collapseToEnd(editorState: EditorState): EditorState {
+  const selection = editorState.getSelection();
+
+  return EditorState.forceSelection(
+    editorState,
+    selection.merge({
+      anchorKey: selection.getEndKey(),
+      focusKey: selection.getEndKey(),
+      anchorOffset: selection.getEndOffset(),
+      focusOffset: selection.getEndOffset(),
+    })
+  );
+}
+
+export function getCurrentEntityKey(editorState: EditorState): ?string {
+  const selection = editorState.getSelection();
+  const anchorKey = selection.getAnchorKey();
+  const contentState = editorState.getCurrentContent();
+  const anchorBlock = contentState.getBlockForKey(anchorKey);
+  const offset = selection.anchorOffset;
+  const index = selection.isBackward ? offset - 1 : offset;
+  return anchorBlock.getEntityAt(index);
+}
+
+export function getCurrentEntity(editorState: EditorState): ?DraftEntityInstance {
+  const contentState = editorState.getCurrentContent();
+  const entityKey = this.getCurrentEntityKey(editorState);
+  return entityKey ? contentState.getEntity(entityKey) : null;
+}
+
+export function hasEntity(editorState: EditorState, entityType: string): boolean {
+  const entity = this.getCurrentEntity(editorState);
+  return entity && entity.getType() === entityType;
+}
