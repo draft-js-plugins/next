3030b4841d68c419f7c5857b4269f490c0120dc1
freedomlang <blackywkl@gmail.com>
2018-09-29 22:15:51 +0800

    Initialize static toolbar plugin

---
+++ packages/djs-static-toobar/rollup.config.js
@@ -0,0 +1,36 @@
+import babel from 'rollup-plugin-babel'
+import commonjs from 'rollup-plugin-commonjs'
+import external from 'rollup-plugin-peer-deps-external'
+import postcss from 'rollup-plugin-postcss'
+import resolve from 'rollup-plugin-node-resolve'
+import url from 'rollup-plugin-url'
+
+import pkg from './package.json'
+
+export default {
+  external: ['djs-editor'],
+  input: 'src/index.js',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs'
+    },
+    {
+      file: pkg.module,
+      format: 'es'
+    }
+  ],
+  plugins: [
+    external(),
+    postcss({
+      modules: true,
+      extract: true,
+    }),
+    url(),
+    babel({
+      exclude: 'node_modules/**'
+    }),
+    resolve(),
+    commonjs()
+  ]
+}
