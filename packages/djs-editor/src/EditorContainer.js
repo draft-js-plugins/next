4c84cc41b7ca58cfd865ace85e2f6a41f955fda4
Hosmel Quintana <hosmelq@gmail.com>
2018-10-01 22:11:23 -0600

    call plugin onChange method

--- packages/djs-editor/src/EditorContainer.js
+++ packages/djs-editor/src/EditorContainer.js
@@ -171,7 +171,7 @@ export default class EditorContainer extends Component<Props, State> {
 
   onChange = editorState => {
     this.props.onChange(editorState)
-    // this.eventCallback('onChange', editorState)
+    this.eventCallback('onChange', editorState)
   }
 
   render() {
@@ -179,7 +179,7 @@ export default class EditorContainer extends Component<Props, State> {
       // plugin specific
       pluginMethods: {
         registerPlugin: this.registerPlugin,
-        setEditorState: this.onChange,
+        setEditorState: editorState => this.props.onChange(editorState),
         setEditorProps: editorProps => this.setState({ editorProps })
       },
       editorProps: {
