f037e812318f377c7c0b8c6ba4fdeed8c37ee30b
freedomlang <blackywkl@gmail.com>
2018-10-03 23:54:13 +0800

    djs-buttons: Initialize redo button

---
+++ packages/djs-buttons/src/components/RedoButton/index.js
@@ -0,0 +1,40 @@
+// @flow
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { EditorState } from 'draft-js';
+import { withEditorContext } from 'djs-editor';
+import unionClassNames from 'union-class-names';
+
+class RedoButton extends Component {
+
+  onClick = (event) => {
+    const {
+      editorProps: { editorState },
+      pluginMethods: { setEditorState }
+    } = this.props;
+
+    event.stopPropagation();
+    setEditorState(EditorState.redo(editorState));
+  };
+
+  render() {
+    const { children, className } = this.props;
+    const combinedClassName = unionClassNames(buttonStyles.button, className);
+    const {
+      editorProps: { editorState }
+    } = this.props;
+    return (
+      <button
+        disabled={
+          !editorState ||
+          editorState.getRedoStack().isEmpty()
+        }
+        type="button"
+        onClick={this.onClick}
+        className={combinedClassName}
+      >↻</button>
+    );
+  }
+}
+
+export default withEditorContext(RedoButton);
