3c12ff5b3760df0c0b3e12f05239fc6b23e4f3cf
freedomlang <blackywkl@gmail.com>
2018-10-21 17:04:01 +0800

    feat(utils): move charCount, lineCount and wordCound into utils

--- packages/counter/src/utils/getLineCount.js
+++
@@ -1,4 +0,0 @@
-export default function getLineCount(editorState) {
-  const blockArray = editorState.getCurrentContent().getBlocksAsArray()
-  return blockArray ? blockArray.length : null
-}
--- packages/counter/src/utils/getWordCount.js
+++
@@ -1,7 +0,0 @@
-export default function getWordCount(editorState) {
-  const plainText = editorState.getCurrentContent().getPlainText('')
-  const regex = /(?:\r\n|\r|\n)/g // new line, carriage return, line feed
-  const cleanString = plainText.replace(regex, ' ').trim() // replace above characters w/ space
-  const wordArray = cleanString.match(/\S+/g) // matches words according to whitespace
-  return wordArray ? wordArray.length : 0
-}
