a8d17543f3818ec03fbaabc5f2cead0c0aa8e16b
freedomlang <blackywkl@gmail.com>
2018-10-31 23:11:34 +0800

    add example for divider

---
+++ examples/divider-example/src/index.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+import { EditorState, convertFromRaw } from 'draft-js'
+import { EditorContainer, Editor } from '@djsp/core'
+import AtomicBlock from '@djsp/atomic-block'
+import '@djsp/atomic-block/dist/index.css'
+import './styles.css'
+
+const rawContent = {
+  blocks: [
+    {
+      text: 'Here is the divider!',
+    },
+    {
+      type: 'atomic',
+      text: ' ',
+      entityRanges: [
+        {
+          key: 0,
+          length: 1,
+          offset: 0,
+        },
+      ],
+    },
+    {
+      text: 'You can add another divider below.',
+    },
+  ],
+  entityMap: {
+    0: {
+      mutability: 'IMMUTABLE',
+      type: 'DIVIDER',
+    },
+  },
+}
+
+class App extends Component {
+  state = {
+    editorState: EditorState.createWithContent(convertFromRaw(rawContent)),
+  }
+
+  onChange = editorState => this.setState({ editorState })
+
+  render() {
+    return (
+      <div>
+        <EditorContainer
+          editorState={this.state.editorState}
+          onChange={this.onChange}>
+          <Editor />
+
+          <AtomicBlock type="divider">
+            {() => <hr className="divider" />}
+          </AtomicBlock>
+        </EditorContainer>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
