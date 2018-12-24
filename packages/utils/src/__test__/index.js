a5d0954a3636d1445943cdeb7fcfee1a0f7d3515
freedomlang <blackywkl@gmail.com>
2018-12-24 20:16:08 +0800

    add test for getWordCount and add non-unicode test for getCharCount

--- packages/utils/src/__test__/index.js
+++ packages/utils/src/__test__/index.js
@@ -1,8 +1,8 @@
 import { expect } from 'chai'
 import { EditorState, ContentState } from 'draft-js'
-import { getCharCount } from '../index'
+import { getCharCount, getWordCount } from '../index'
 
-describe('char Counter', () => {
+describe('char Counter(non-unicode)', () => {
   const createEditorStateFromText = text => {
     const contentState = ContentState.createFromText(text)
     return EditorState.createWithContent(contentState)
@@ -12,6 +12,34 @@ describe('char Counter', () => {
     const editorState = createEditorStateFromText('One')
 
     const result = getCharCount(editorState)
+    expect(result).to.equal(3)
+  })
+})
+
+describe('char Counter(unicode)', () => {
+  const createEditorStateFromText = text => {
+    const contentState = ContentState.createFromText(text)
+    return EditorState.createWithContent(contentState)
+  }
+
+  it('create editorState and counts 1 char', () => {
+    const editorState = createEditorStateFromText('一')
+
+    const result = getCharCount(editorState)
+    expect(result).to.equal(1)
+  })
+})
+
+describe('word Counter', () => {
+  const createEditorStateFromText = text => {
+    const contentState = ContentState.createFromText(text)
+    return EditorState.createWithContent(contentState)
+  }
+
+  it('create editorState and counts 3 word', () => {
+    const editorState = createEditorStateFromText('One two three')
+
+    const result = getWordCount(editorState)
     expect(result).to.equal(3)
   })
 })
