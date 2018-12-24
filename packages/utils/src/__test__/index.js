dc5925d8875492d2dde326b0b06477816ceda5dd
freedomlang <blackywkl@gmail.com>
2018-12-24 20:35:12 +0800

    add test for getLineCount in utils

--- packages/utils/src/__test__/index.js
+++ packages/utils/src/__test__/index.js
@@ -1,45 +1,52 @@
 import { expect } from 'chai'
 import { EditorState, ContentState } from 'draft-js'
-import { getCharCount, getWordCount } from '../index'
+import { getCharCount, getWordCount, getLineCount } from '../index'
 
-describe('char Counter(non-unicode)', () => {
+describe('Char Counter', () => {
   const createEditorStateFromText = text => {
     const contentState = ContentState.createFromText(text)
     return EditorState.createWithContent(contentState)
   }
 
-  it('create editorState and counts 3 char', () => {
+  it('create editorState and counts 3 characters', () => {
     const editorState = createEditorStateFromText('One')
+
+    const result = getCharCount(editorState)
+    expect(result).to.equal(3)
+  })
+
+  it('create editorState and counts 3 unicode characters', () => {
+    const editorState = createEditorStateFromText('😁😂😃')
 
     const result = getCharCount(editorState)
     expect(result).to.equal(3)
   })
 })
 
-describe('char Counter(unicode)', () => {
+describe('Word Counter', () => {
   const createEditorStateFromText = text => {
     const contentState = ContentState.createFromText(text)
     return EditorState.createWithContent(contentState)
   }
 
-  it('create editorState and counts 1 char', () => {
-    const editorState = createEditorStateFromText('一')
-
-    const result = getCharCount(editorState)
-    expect(result).to.equal(1)
-  })
-})
-
-describe('word Counter', () => {
-  const createEditorStateFromText = text => {
-    const contentState = ContentState.createFromText(text)
-    return EditorState.createWithContent(contentState)
-  }
-
-  it('create editorState and counts 3 word', () => {
+  it('create editorState and counts 3 words', () => {
     const editorState = createEditorStateFromText('One two three')
 
     const result = getWordCount(editorState)
+    expect(result).to.equal(3)
+  })
+})
+
+describe('Line Counter', () => {
+  const createEditorStateFromText = text => {
+    const contentState = ContentState.createFromText(text)
+    return EditorState.createWithContent(contentState)
+  }
+
+  it('create editorState and counts 3 lines', () => {
+    const editorState = createEditorStateFromText('One\ntwo\nthree')
+
+    const result = getLineCount(editorState)
     expect(result).to.equal(3)
   })
 })
