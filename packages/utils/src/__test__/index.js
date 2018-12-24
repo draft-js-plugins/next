4ed91934cfdf01e09dd7d0367403a3e9fe74d8d8
freedomlang <blackywkl@gmail.com>
2018-12-24 21:24:29 +0800

    add test for hasEntity

--- packages/utils/src/__test__/index.js
+++ packages/utils/src/__test__/index.js
@@ -1,6 +1,61 @@
 import { expect } from 'chai'
-import { EditorState, ContentState } from 'draft-js'
-import { getCharCount, getWordCount, getLineCount } from '../index'
+import {
+  EditorState,
+  ContentState,
+  convertFromRaw,
+  SelectionState,
+} from 'draft-js'
+import { getCharCount, getWordCount, getLineCount, hasEntity } from '../index'
+
+describe('Has Entity', () => {
+  const createEditorStateFromRaw = raw => {
+    const contentState = convertFromRaw(raw)
+    return EditorState.createWithContent(contentState)
+  }
+
+  it('create editorState and check if has Entity', () => {
+    const rawContent = {
+      blocks: [
+        {
+          text: 'Hey there duder',
+        },
+        {
+          type: 'atomic',
+          text: ' ',
+          entityRanges: [
+            {
+              key: 0,
+              length: 1,
+              offset: 0,
+            },
+          ],
+        },
+      ],
+      entityMap: {
+        0: {
+          data: {
+            title: 'Kitten',
+            src: 'https://placekitten.com/200/200',
+          },
+          mutability: 'IMMUTABLE',
+          type: 'IMAGE',
+        },
+      },
+    }
+    const editorState = createEditorStateFromRaw(rawContent)
+    const blockKey = editorState
+      .getCurrentContent()
+      .getLastBlock()
+      .getKey()
+    const selectionState = SelectionState.createEmpty(blockKey)
+    const updatedEditorState = EditorState.forceSelection(
+      editorState,
+      selectionState
+    )
+    const result = hasEntity(updatedEditorState, 'IMAGE')
+    expect(result).to.equal(true)
+  })
+})
 
 describe('Char Counter', () => {
   const createEditorStateFromText = text => {
