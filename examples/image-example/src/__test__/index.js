7773415e95483543ea25747133dc9a4ff7dd666f
freedomlang <blackywkl@gmail.com>
2019-01-27 23:38:17 +0800

    add test for delete image

--- examples/image-example/src/__test__/index.js
+++ examples/image-example/src/__test__/index.js
@@ -11,14 +11,19 @@ afterEach(cleanup)
 describe('Tests for render image via @djsp/atomic-block', () => {
   const { container } = render(<App />)
   const imageEl = container.querySelector('img')
+  const clickedImage = imageEl[0]
 
   it('It should have an image in editor', () => {
     expect(imageEl).to.have.length(1)
   })
 
   it('Display border when the image was clicked', async () => {
-    const clickedImage = imageEl[0]
     fireEvent.click(clickedImage)
     await wait(() => expect(clickedImage).to.have.class('focused'))
+  })
+
+  it('Image should be deleted', async () => {
+    fireEvent.keyDown(clickedImage, { key: 'Backspace', code: 8})
+    await wait(() => expect(container.querySelector('img')).should.be.empty)
   })
 })
